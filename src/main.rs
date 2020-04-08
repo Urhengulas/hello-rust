a5e5eeed9b0244d8a9cb6880bfe1dcbd30ba0d3d
Urhengulas <johann.hemmann@code.berlin>
2020-04-08 23:35:48 +0200

    Tuples
    https://doc.rust-lang.org/stable/rust-by-example/primitives/tuples.html

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,63 @@
 use std::fmt;
 
-#[derive(Debug)]
-struct Color {
-	red: u8,
-	green: u8,
-	blue: u8,
+// Tuples can be used as function arguments and as return values
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+	// `let` can be used to bind the members of a tuple to variables
+	let (integer, boolean) = pair;
+	return (boolean, integer);
 }
-impl fmt::Display for Color {
+
+// The following struct is for the activity.
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+impl fmt::Display for Matrix {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "RGB ")?;
-		write!(f, "({}, {}, {}) ", self.red, self.green, self.blue)?;
-		write!(f, "0x{:02X}{:02X}{:02X}", self.red, self.green, self.blue)
+		writeln!(f, "Matrix:")?;
+		writeln!(f, "( {} {} )", self.0, self.1)?;
+		writeln!(f, "( {} {} )", self.2, self.3)?;
+		writeln!(f, "Transpose:")?;
+		writeln!(f, "( {} {} )", self.0, self.2)?;
+		write!(f, "( {} {} )", self.1, self.3)
 	}
 }
 
 fn main() {
-	for color in [
-		Color {
-			red: 128,
-			green: 255,
-			blue: 90,
-		},
-		Color {
-			red: 0,
-			green: 3,
-			blue: 254,
-		},
-		Color {
-			red: 0,
-			green: 0,
-			blue: 0,
-		},
-	]
-	.iter()
-	{
-		// Switch this to use {} once you've added an implementation
-		// for fmt::Display.
-		println!("{}", *color);
-	}
+	// A tuple with a bunch of different types
+	let long_tuple = (
+		1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+	);
+
+	// Values can be extracted from the tuple using tuple indexing
+	println!("long tuple first value: {}", long_tuple.0);
+	println!("long tuple second value: {}", long_tuple.1);
+
+	// Tuples can be tuple members
+	let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+	// Tuples are printable
+	println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+	// But long Tuples cannot be printed
+	// let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+	// println!("too long tuple: {:?}", too_long_tuple);
+	// TODO ^ Uncomment the above 2 lines to see the compiler error
+
+	let pair = (1, true);
+	println!("pair is {:?}", pair);
+
+	println!("the reversed pair is {:?}", reverse(pair));
+
+	// To create one element tuples, the comma is required to tell them apart
+	// from a literal surrounded by parentheses
+	println!("one element tuple: {:?}", (5u32,));
+	println!("just an integer: {:?}", (5u32));
+
+	//tuples can be destructured to create bindings
+	let tuple = (1, "hello", 4.5, true);
+
+	let (a, b, c, d) = tuple;
+	println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+	let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+	println!("{}", matrix);
 }
