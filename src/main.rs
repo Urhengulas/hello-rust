fa13421d660bbfb2fd81695e1ecd4ede59df8f4e
Urhengulas <johann.hemmann@code.berlin>
2020-04-10 21:02:18 +0200

    Structures
    https://doc.rust-lang.org/stable/rust-by-example/custom_types/structs.html

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
-use std::mem;
+// A struct with two fields
+#[derive(Debug)]
+struct Point {
+	x: f32,
+	y: f32,
+}
 
-// This function borrows a slice
-fn analyze_slice(slice: &[i32]) {
-	println!("first element of the slice: {}", slice[0]);
-	println!("the slice has {} elements", slice.len());
+// Structs can be reused as fields of another struct
+#[derive(Debug)]
+struct Rectangle {
+	// A rectangle can be specified by where the top left and bottom right
+	// corners are in space.
+	top_left: Point,
+	bottom_right: Point,
+}
+
+fn rect_area(r: Rectangle) -> f64 {
+	let Rectangle {
+		top_left: Point {
+			x: left_x,
+			y: top_y,
+		},
+		bottom_right: Point {
+			x: right_x,
+			y: bottom_y,
+		},
+	} = r;
+	let (d_x, d_y) = (right_x - left_x, top_y - bottom_y);
+	return (d_x * d_y).abs().into();
+}
+
+fn square(p: Point, a: f32) -> Rectangle {
+	return Rectangle {
+		top_left: Point { x: p.x, y: p.y + a },
+		bottom_right: Point { x: p.x + a, y: p.y },
+	};
 }
 
 fn main() {
-	// Fixed-size array (type signature is superfluous)
-	let xs: [i32; 5] = [1, 2, 3, 4, 5];
+	let rectangle = Rectangle {
+		bottom_right: Point { x: 1.2, y: -3.4 },
+		top_left: Point { x: 5.6, y: 7.8 },
+	};
+	let a_reactangle = rect_area(dbg!(rectangle));
+	println!("area: {} AU\n", a_reactangle);
 
-	// All elements can be initialized to the same value
-	let ys: [i32; 500] = [0; 500];
-
-	// Indexing starts at 0
-	println!("first element of the array: {}", xs[0]);
-	println!("second element of the array: {}", xs[1]);
-
-	// `len` returns the size of the array
-	println!("array size: {}", xs.len());
-
-	// Arrays are stack allocated
-	println!("array occupies {} bytes", mem::size_of_val(&xs));
-
-	// Arrays can be automatically borrowed as slices
-	println!("borrow the whole array as a slice");
-	analyze_slice(&xs);
-
-	// Slices can point to a section of an array
-	// They are of the form [starting_index..ending_index]
-	// starting_index is the first position in the slice
-	// ending_index is one more than the last position in the slice
-	println!("borrow a section of the array as a slice");
-	analyze_slice(&ys[1..4]);
-
-	// Out of bound indexing causes compile error
-	// println!("{}", xs[5]);
+	let rectangle_sq = square(Point { x: 5.6, y: 7.8 }, 9.1);
+	println!("square rectangle: {:?}", rectangle_sq);
+	let a_reactangle_sq = rect_area(rectangle_sq);
+	println!("area: {} AU", a_reactangle_sq);
 }
