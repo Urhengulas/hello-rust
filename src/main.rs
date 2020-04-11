15b0e5b9446714663bf788134d4154f7079a248a
Urhengulas <johann.hemmann@code.berlin>
2020-04-11 17:44:51 +0200

    Enum - C-like
    https://doc.rust-lang.org/stable/rust-by-example/custom_types/enum/c_like.html

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,26 @@
-// A struct with two fields
-#[derive(Debug)]
-struct Point {
-	x: f32,
-	y: f32,
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+// enum with implicit discriminator (starts at 0)
+enum Abrafaxe {
+	Abrax,
+	Bebrax,
+	Califax,
 }
 
-// Structs can be reused as fields of another struct
-#[derive(Debug)]
-struct Rectangle {
-	// A rectangle can be specified by where the top left and bottom right
-	// corners are in space.
-	top_left: Point,
-	bottom_right: Point,
-}
-
-fn rect_area(r: Rectangle) -> f64 {
-	let Rectangle {
-		top_left: Point {
-			x: left_x,
-			y: top_y,
-		},
-		bottom_right: Point {
-			x: right_x,
-			y: bottom_y,
-		},
-	} = r;
-	let (d_x, d_y) = (right_x - left_x, top_y - bottom_y);
-	return (d_x * d_y).abs().into();
-}
-
-fn square(p: Point, a: f32) -> Rectangle {
-	return Rectangle {
-		top_left: Point { x: p.x, y: p.y + a },
-		bottom_right: Point { x: p.x + a, y: p.y },
-	};
+// enum with explicit discriminator
+enum Color {
+	Red = 0xff0000,
+	Green = 0x00ff00,
+	Blue = 0x0000ff,
 }
 
 fn main() {
-	let rectangle = Rectangle {
-		bottom_right: Point { x: 1.2, y: -3.4 },
-		top_left: Point { x: 5.6, y: 7.8 },
-	};
-	let a_reactangle = rect_area(dbg!(rectangle));
-	println!("area: {} AU\n", a_reactangle);
+	// `enums` can be cast as integers.
+	println!("Abrax is {}", Abrafaxe::Abrax as i32);
+	println!("Califax is {}", Abrafaxe::Califax as i32);
 
-	let rectangle_sq = square(Point { x: 5.6, y: 7.8 }, 9.1);
-	println!("square rectangle: {:?}", rectangle_sq);
-	let a_reactangle_sq = rect_area(rectangle_sq);
-	println!("area: {} AU", a_reactangle_sq);
+	println!("\nRoses are #{:06x}", Color::Red as i32);
+	println!("Violets are #{:06x}", Color::Blue as i32);
+	println!("Sugar is sweet,\nAnd so are you.")
 }
