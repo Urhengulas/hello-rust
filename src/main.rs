5489245619f16d5b4bcea48bc543d7473001ea0d
Urhengulas <johann.hemmann@code.berlin>
2020-04-13 16:58:43 +0200

    - Replace prints with debug

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ use rand::Rng;
 fn main() {
 	println!("Guess the number!");
 
-	let secret_number = rand::thread_rng().gen_range(1, 101);
-
-	println!("The secret number is: {}", secret_number);
+	let secret_number = dbg!(rand::thread_rng().gen_range(1, 101));
 
 	println!("Please input your guess.");
 
@@ -23,9 +21,7 @@ fn main() {
 
 	let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-	println!("You guessed: {}", guess);
-
-	match guess.cmp(&secret_number) {
+	match dbg!(guess).cmp(dbg!(&secret_number)) {
 		Ordering::Less => println!("Too small!"),
 		Ordering::Greater => println!("Too big!"),
 		Ordering::Equal => println!("You win!"),
