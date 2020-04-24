e1cb1ba4966d9b6b942e208c79c643b91948fa50
Urhengulas <johann.hemmann@code.berlin>
2020-04-24 13:40:02 +0200

    Control Flow
    https://doc.rust-lang.org/book/ch03-05-control-flow.html

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use std::time::SystemTime;
+
 fn main() {
-	check_divisiblity(22);
+	let time = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+		Ok(n) => n.as_secs(),
+		Err(_) => panic!("SystemTime before UNIX EPOCH"),
+	};
+	check_divisiblity(time);
 }
 
-fn check_divisiblity(number: i32) {
+fn check_divisiblity(number: u64) {
 	if number % 5 == 0 {
-		println!("number is divisible by 5");
+		println!("{} is divisible by 5", number);
 	} else if number % 4 == 0 {
-		println!("number is divisible by 4");
+		println!("{} is divisible by 4", number);
 	} else if number % 3 == 0 {
-		println!("number is divisible by 3");
+		println!("{} is divisible by 3", number);
 	} else if number % 2 == 0 {
-		println!("number is divisible by 2");
+		println!("{} is divisible by 2", number);
 	} else {
-		println!("number is not divisible by 2, 3, 4 or 5")
+		println!("{} is not divisible by 2, 3, 4 or 5", number)
 	}
 }
