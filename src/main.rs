eeeb57a4c4c9f6ad9b99c05f33d32a4c21e82597
Urhengulas <johann.hemmann@code.berlin>
2020-04-24 13:20:53 +0200

    Control Flow
    https://doc.rust-lang.org/book/ch03-05-control-flow.html

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 fn main() {
-	let x = 5;
-
-	let y = {
-		let x = x + 3;
-		x + 1
-	};
-
-	println!("The value of y² is: {}", square(y));
+	check_divisiblity(22);
 }
 
-fn square(x: i32) -> i32 {
-	x * x
+fn check_divisiblity(number: i32) {
+	if number % 5 == 0 {
+		println!("number is divisible by 5");
+	} else if number % 4 == 0 {
+		println!("number is divisible by 4");
+	} else if number % 3 == 0 {
+		println!("number is divisible by 3");
+	} else if number % 2 == 0 {
+		println!("number is divisible by 2");
+	} else {
+		println!("number is not divisible by 2, 3, 4 or 5")
+	}
 }
