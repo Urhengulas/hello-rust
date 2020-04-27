1865c72d07bb40d9c33ac66548cdaad76b380786
Urhengulas <johann.hemmann@code.berlin>
2020-04-27 14:28:50 +0200

    Defining and Instantiating Structs
    https://doc.rust-lang.org/book/ch05-01-defining-structs.html

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use std::string::String;
-
 fn main() {
-	let sentence = String::from("Hello world eyy");
-	let first_word = get_first_word(&sentence);
-	dbg!(first_word);
-}
+	// structs
+	{
+		#[derive(Debug)]
+		struct User {
+			username: String,
+			email: String,
+			sign_in_count: u64,
+			active: bool,
+		}
 
-// TODO: make this function a oneliner
-fn get_first_word(s: &String) -> String {
-	let word_list: Vec<&str> = s.split(' ').collect();
-	String::from(word_list[0])
+		let user1 = User {
+			email: String::from("someone@example.com"),
+			username: String::from("someusername123"),
+			active: true,
+			sign_in_count: 1,
+		};
+		let user2 = User {
+			email: String::from("mail@lukasmüller.com"),
+			username: String::from("mToTheLukas"),
+			..user1
+		};
+		dbg!((user1, user2));
+	}
+
+	// tuple structs
+	{
+		#[derive(Debug)]
+		struct Color(i32, i32, i32);
+		#[derive(Debug)]
+		struct Point(i32, i32, i32);
+
+		let black = Color(0, 0, 0);
+		let origin = Point(0, 0, 0);
+		dbg!((black, origin));
+	}
 }
