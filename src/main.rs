d9358a3650414f433ac6eaa73031969c31a29776
Urhengulas <johann.hemmann@code.berlin>
2020-04-27 14:10:12 +0200

    The Slice Type
    * get_first_word is actually not using slices
    
    https://doc.rust-lang.org/book/ch04-03-slices.html

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
-use std::thread;
-use std::time::{Duration, SystemTime};
+use std::string::String;
 
 fn main() {
-	let unix_time = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-		Ok(n) => n.as_secs(),
-		Err(_) => panic!("SystemTime before UNIX EPOCH"),
-	};
-	dbg!(unix_time);
-
-	// quintuple counter until it is greater or equal to unix_time at start of program
-	let mut counter = 1;
-	let result = loop {
-		if dbg!(counter) >= unix_time {
-			break counter;
-		};
-		counter *= 5;
-		sleep(0.1);
-	};
-	dbg!(unix_time);
-	dbg!(result);
+	let sentence = String::from("Hello world eyy");
+	let first_word = get_first_word(&sentence);
+	dbg!(first_word);
 }
 
-fn sleep(time: f64) {
-	thread::sleep(Duration::from_secs_f64(time));
+// TODO: make this function a oneliner
+fn get_first_word(s: &String) -> String {
+	let word_list: Vec<&str> = s.split(' ').collect();
+	String::from(word_list[0])
 }
