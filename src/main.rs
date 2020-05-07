865e16b6cc172bd72678ba86a800b889c729c55c
Urhengulas <johann.hemmann@code.berlin>
2020-05-07 12:34:42 +0200

    The `match` Control Flow Operator
    https://doc.rust-lang.org/book/ch06-02-match.html

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,33 @@
 #[derive(Debug)]
-struct Cuboid {
-	height: u32,
-	width: u32,
-	depth: u32,
+enum EuState {
+	France,
+	Italy,
+	Germany,
+	// --snip--
 }
-impl Cuboid {
-	// methods
-	fn volume(&self) -> u32 {
-		self.height * self.width * self.depth
-	}
-	fn can_hold(&self, other: &Cuboid) -> bool {
-		self.height >= other.height && self.width >= other.width && self.depth >= other.depth
-	}
-	// associated functions
-	fn create_dice(size: u32) -> Cuboid {
-		Cuboid {
-			height: size,
-			width: size,
-			depth: size,
-		}
+
+enum Coin {
+	Cent,
+	Cent50,
+	Euro(EuState),
+	Euro2,
+}
+
+fn value_in_cents(coin: Option<Coin>) -> Option<f64> {
+	match coin {
+		Some(c) => match c {
+			Coin::Cent => Some(0.01),
+			Coin::Cent50 => Some(0.5),
+			Coin::Euro(state) => {
+				println!("A 1€ coin from {:?}!", state);
+				Some(1.0)
+			}
+			Coin::Euro2 => Some(2.0),
+		},
+		None => None,
 	}
 }
 
 fn main() {
-	let c1 = Cuboid {
-		height: 30,
-		width: 50,
-		depth: 20,
-	};
-	dbg!(c1.volume());
-	dbg!(c1.can_hold(&Cuboid {
-		height: 30,
-		width: 45,
-		depth: 20
-	}));
-	dbg!(c1.can_hold(&Cuboid {
-		height: 35,
-		width: 50,
-		depth: 20
-	}));
-	let dice = Cuboid::create_dice(20);
-	dbg!(dice.volume());
-	dbg!(c1.can_hold(&dice));
+	dbg!(value_in_cents(Some(Coin::Euro(EuState::Germany))));
 }
