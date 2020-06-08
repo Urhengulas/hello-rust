53b484d8f06fca45b6fdfa48fa05d3c93869dc8f
Urhengulas <johann.hemmann@code.berlin>
2020-06-08 19:23:22 +0200

    Shortcuts for Panic on Error: unwrap and expect
    https://doc.rust-lang.org/stable/book/ch09-02-recoverable-errors-with-result.html#shortcuts-for-panic-on-error-unwrap-and-expect

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
-#[derive(Debug)]
-enum EuState {
-	France,
-	Italy,
-	Germany,
-	// --snip--
-}
-
-enum Coin {
-	Cent,
-	Cent50,
-	Euro(EuState),
-	Euro2,
-}
-
-fn value_in_cents(coin: Option<Coin>) -> Option<f64> {
-	match coin {
-		Some(c) => match c {
-			Coin::Cent => Some(0.01),
-			Coin::Cent50 => Some(0.5),
-			Coin::Euro(state) => {
-				println!("A 1€ coin from {:?}!", state);
-				Some(1.0)
-			}
-			Coin::Euro2 => Some(2.0),
-		},
-		None => None,
-	}
-}
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::ErrorKind;
 
 fn main() {
-	dbg!(value_in_cents(Some(Coin::Euro(EuState::Germany))));
+	let path = "hey.md";
+	let file = match File::open(path) {
+		Ok(f) => f,
+		Err(e) => File::create(path).expect(&*format!("Problem creating the file \"{}\"", path)),
+	};
+	dbg!(&file);
 }
