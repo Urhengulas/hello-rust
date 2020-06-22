3c049e0aca86d98491a88d57e46bf0cf9a3f91c5
Urhengulas <johann.hemmann@code.berlin>
2020-06-22 22:53:06 +0200

    Storing Closures Using Generic Parameters and the Fn Traits
    https://doc.rust-lang.org/book/ch13-01-closures.html#storing-closures-using-generic-parameters-and-the-fn-traits

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,60 @@
-fn largest<T>(list: &[T]) -> &T
-where
-	T: PartialOrd,
-{
-	let mut largest = &list[0];
+use std::env;
+use std::thread;
+use std::time::Duration;
 
-	for item in list {
-		if item > largest {
-			largest = item;
+struct Cacher<T: Fn(u32) -> u32> {
+	calculation: T,
+	value: Option<u32>,
+}
+impl<T> Cacher<T>
+where
+	T: Fn(u32) -> u32,
+{
+	fn new(calculation: T) -> Cacher<T> {
+		Cacher {
+			calculation,
+			value: None,
 		}
 	}
 
-	largest
+	fn value(&mut self, arg: u32) -> u32 {
+		match self.value {
+			Some(v) => v,
+			None => {
+				let v = (self.calculation)(arg);
+				self.value = Some(v);
+				v
+			}
+		}
+	}
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+	let mut expensive_result = Cacher::new(|num| {
+		println!("calculating slowly...");
+		thread::sleep(Duration::from_secs(2));
+		num
+	});
+
+	if intensity < 25 {
+		println!("Today, do {} pushups!", expensive_result.value(intensity));
+		println!("Next, do {} situps!", expensive_result.value(intensity));
+	} else {
+		if random_number == 3 {
+			println!("Take a break today! Remember to stay hydrated!");
+		} else {
+			println!(
+				"Today, run for {} minutes!",
+				expensive_result.value(intensity)
+			);
+		}
+	}
 }
 
 fn main() {
-	let number_list = vec![34, 50, 25, 100, 65];
+	let args: Vec<String> = env::args().collect();
+	let simulated_user_specified_value = args[1].parse::<u32>().unwrap();
+	let simulated_random_number = args[2].parse::<u32>().unwrap();
 
-	let result = largest(&number_list);
-	println!("The largest number is {}", result);
-
-	let char_list = vec!['y', 'm', 'a', 'q'];
-
-	let result = largest(&char_list);
-	println!("The largest char is {}", result);
+	generate_workout(simulated_user_specified_value, simulated_random_number);
 }
