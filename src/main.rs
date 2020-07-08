1c3cb2d42a7e449cfd1c0c64ab2a8a191430cab7
Urhengulas <johann.hemmann@code.berlin>
2020-07-08 10:53:20 +0200

    Function Pointers
    https://doc.rust-lang.org/book/ch19-05-advanced-functions-and-closures.html#function-pointers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#[derive(Debug)]
+enum Status {
+	Value(u32),
+	Stop,
+}
 fn main() {
-	print!("forever ");
-
-	let _a = loop {
-		println!("and ever ");
-	};
+    let mut list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+    list_of_statuses.push(Status::Stop);
+	dbg!(list_of_statuses);
 }
